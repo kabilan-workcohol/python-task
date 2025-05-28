bca99cc9973b53eab601672594fb227d692b0513
kabilan-workcohol <kabilan@workcohol.com>
2025-05-28 19:25:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+s = "kabil"
+for i in s:
+    print(i)
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+i=int(input("enter the i value: "))
+if (i > 0):
+    print("i is positive")
+else:
+    print("i is 0 or Negative")
+age = 25
+exp = 10
+if age > 23 and exp > 8:
+    print("Eligible.")
+else:
+    print("Not eligible.")
